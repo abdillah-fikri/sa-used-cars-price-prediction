45b27a0584d7675eedc3c815e66a15bf584034a3
abdillah-fikri <abdillah.fikri14@gmail.com>
2020-10-18 19:20:57 +0700

    Menambahkan file template

---
+++ markdown/data_prep.md
@@ -0,0 +1,10 @@
+---
+jupyter:
+  jupytext:
+    formats: notebooks//ipynb,markdown//md,scripts/preparation//py:percent
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.2'
+      jupytext_version: 1.6.0
+---
---
+++ scripts/modeling/modeling.py
@@ -0,0 +1,10 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: notebooks//ipynb,markdown//md,scripts/modeling//py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+# ---
